546f48a9743d672f385913290d8a43bd12bfaea1
abhishekgaire1 <abhishek.gaire@stonybrook.edu>
2022-10-21 20:05:16 -0400

    Updated models with Map models

---
+++ server/models/Map.js
@@ -0,0 +1,83 @@
+const mongoose = require('mongoose');
+const MapSchema = new mongoose.Schema({
+    backgroundColor: {
+        type: String,
+    },
+    class: {
+        type: String,
+    },
+    compressionLevel: {
+        type: Number,
+    },
+    height: {
+        type: Number,
+    },
+    hexSideLength: {
+        type: Number,
+    },
+    infinite: {
+        type: Boolean,
+    },
+    layers: {
+        type: [Layer],
+    },
+    nextlayerid: {
+        type: Number,
+    },
+    nextobjectid: {
+        type: String,
+    },
+    orientation: {
+        type: String,
+    },
+    parallaxOriginX: {
+        type: String,
+    },
+    parallaxOriginY: {
+        type: Number,
+        
+    },
+    properties: {
+        type: [Property],
+        
+    },
+    renderorder: {
+        type: String,
+    },
+    staggeraxis: {
+        type: String,
+    },
+    staggerindex: {
+        type: String,
+    },
+    tiledversion: {
+        type: String,
+    },
+    tileheight: {
+        type: Number,
+        
+    },
+    tilesets: {
+        type: [Tileset] 
+        
+    },
+    tilewidth: {
+        type: Number,
+        
+    },
+    type: {
+        type: String,
+        
+    },
+    version: {
+        type: String,
+        
+    },
+    width: {
+        type: Number,
+        
+    }
+    
+});
+
+module.exports = mongoose.model('Map', MapSchema);
