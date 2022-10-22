dd97e45d69bfb965ad4de44d641dee94fbe5a357
Kevin <kevin.duong10@yahoo.com>
2022-10-21 20:02:01 -0400

    Added tileset schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -29,6 +29,36 @@ const ProjectType = new GraphQLObjectType({
     })
 });
 
+const TilesetType = new GraphQLObjectType({
+    name: 'Tileset',
+    fields: () => ({
+        backgroundcolor: {type: String},
+        class: {type: String},
+        columns: {type: number},
+        fillmode: {type: String},
+        firstgid: {type: Number},
+        //grid: {type: Grid},
+        image: {type: String},
+        imageheight: {type: Number},
+        imagewidth: {type: Number},
+        margin: {type: Number},
+        objectalignment: {type: String},
+        properties: {type: [Property]},
+        source: {type: Number},
+        terrains: {type: [Terrain]},
+        tilecount: {type: Number},
+        source: {type: String},
+        tiledversion: {type: String},
+        tilerendersize: {type: String},
+        tiles: {type: [Tile]},
+        tilewidth: {type: Number},
+        transformations: {type: Transformation},
+        transparentcolor: {type: String},
+        type: {type: String},
+        version: {type: String}
+    })
+});
+
 const mutation = new GraphQLObjectType({
     name: 'Mutation',
     fields: {
