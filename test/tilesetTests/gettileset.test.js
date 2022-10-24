13ef42bbb59ab17d28c36f9a6d98fe0bc027e509
DESKTOP-1794Q3H\maxri <maximiliano.rinaldi@stonybrook.edu>
2022-10-24 07:10:58 -0400

    get tileset test added

--- test/tilesetTests/gettileset.test.js
+++ test/tilesetTests/gettileset.test.js
@@ -1,29 +1,29 @@
 require('isomorphic-fetch');
 let tileset = require("./sampleTS.json");
 
-test("Create Tileset", () => {
-   
+test("Get User", () => {
     const expected = {
-        "addTileSet": {
-            "image": "../OneDrive/Documents/lungo/benchmark2/dist/lungo_assets/tilemaps/level1.png"
-          }
+        "getTileset": 
+            {
+                "image": "../OneDrive/Documents/lungo/benchmark2/dist/lungo_assets/tilemaps/level1.png"
+            }
+        
     };
-    
-    
+
     const variables = { input: tileset };
     console.log(`TilesetInput: ${JSON.stringify(variables)}`);
 
-    return fetch('https://taps-backend.herokuapp.com/graphql', {
+    fetch('https://taps-backend.herokuapp.com/graphql', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     // The query we are sending to the GraphQL API
     body: JSON.stringify({ query: 
-        `mutation($input: TileInput){
-            addTileSet(TilesetInput: $input){
+        `{
+            getTileset(_id: "6355c0ac01c51ccdc0256da6
+            ") {
               image,
             }
-          }`,
-          variables 
+          }` 
         })
     })
     .then(res => res.json())
