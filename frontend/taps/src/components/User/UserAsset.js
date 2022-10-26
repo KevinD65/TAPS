002670043325408c4635091feca79f889eab3a61
Kevin Duong <59448080+KevinD65@users.noreply.github.com>
2022-10-26 14:34:22 -0400

    Finished community screen with functional recent searches

--- frontend/taps/src/components/User/UserAsset.js
+++ frontend/taps/src/components/User/UserAsset.js
@@ -1,14 +1,31 @@
 import React from "react";
-
+import Map from '../Assets/Map'
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 
 const UserAsset = () => {
+
+    /*
+    const toggle
+
+    const handleNewAsset = () =>{
+
+    }
+
+
     return (
-        <div className='user-asset'>
-            <h1>User asset</h1>
+
+        
+        
+    <div class="dropdown">
+        <button class="dropbtn">+ New</button>
+        <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
         </div>
-    )
+    </div>
+    )*/
 }
 
 export default UserAsset
