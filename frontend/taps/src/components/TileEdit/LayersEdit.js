e70386e3220c536120ec1354bb7fc60955d6eb83
DESKTOP-1794Q3H\maxri <maximiliano.rinaldi@stonybrook.edu>
2022-10-29 15:07:46 -0400

    added basic UI layout to tile edit screen, still needs the canvas

--- frontend/taps/src/components/TileEdit/LayersEdit.js
+++ frontend/taps/src/components/TileEdit/LayersEdit.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Table,TableRow, TableBody, TableCell,TableContainer,TableHead,Checkbox, Typography} from "@mui/material";
 
-import "./mapEdit.css"
+import "./tileEdit.css"
 
   
 export default function LayersEdit() {
@@ -18,31 +18,33 @@ export default function LayersEdit() {
     </thead>
     <tbody>
         <tr>
-            <td>Mountain</td>
+            <td>Detail</td>
             <td><Checkbox defaultChecked /></td>
         </tr>
         <tr class="active-row">
-        <td>Grass</td>
+        <td>Accent</td>
             <td><Checkbox  defaultChecked /></td>
             
     
         </tr>
         <tr>
-            <td>Water</td>
+            <td>Base color</td>
             
             <td><Checkbox  defaultChecked /></td>
             
         </tr>
 
-                {/*temporary UI for adding and removing layers (required feature)*/}
-                <tr>
+
+        {/*temporary UI for adding and removing layers (required feature)*/}
+        <tr>
             <th>Layer Functions</th>          
         </tr>
 
         <tr>
             <td><button> add </button> <button> remove </button></td>
         </tr>
-        
+
+
     </tbody>
 </table>
     
