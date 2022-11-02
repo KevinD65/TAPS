1911e0b51ffd0711ac535ffa6c01e6231cd6a116
Anmol Singh <ASingh2320@outlook.com>
2022-11-02 02:11:58 -0400

    moved map types to separate file

---
+++ server/schema/types/MapType.js
@@ -0,0 +1,42 @@
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList, GraphQLNonNull, GraphQLEnumType, GraphQLInt, GraphQLInputObjectType, GraphQLFloat, GraphQLBoolean, GraphQLScalarType } = require('graphql');
+const MapType = new GraphQLObjectType({
+    name: 'Map',
+    fields: () => ({
+        id: {type: GraphQLID},
+        ownerID: {type: GraphQLInt},
+        backgroundColor: {type: GraphQLString},
+        class: {type: GraphQLString},
+        compressionLevel: {type: GraphQLFloat},
+        height: {type: GraphQLFloat},
+        hexSideLength: {type: GraphQLFloat},
+        infinite: {type: GraphQLBoolean},
+
+        /*
+        layers: {
+            type: GraphQLList(LayerType),
+            resolve(parent, args){
+                return Layer.find({parentID: parent.id});
+            }
+        },
+        */
+        nextlayerid: {type: GraphQLInt},
+        nextobjectid: {type: GraphQLString},
+        orientation: {type: GraphQLString},
+        parallaxOriginX: {type: GraphQLString},
+        parallaxOriginY: {type: GraphQLFloat},
+        //properties: {type: [Property]},
+        renderorder: {type: GraphQLString},
+        staggeraxis: {type: GraphQLString},
+        staggerindex: {type: GraphQLString},
+        tiledversion: {type: GraphQLString},
+        tileheight: {type: GraphQLFloat},
+        //tilesets: {type: GraphQLList(TilesetType)},
+        tilewidth: {type: GraphQLFloat},
+        type: { type: GraphQLString},
+        version: {type: GraphQLString},
+        width: {type: GraphQLFloat}
+    })
+});
+
+
+module.exports = MapType;
